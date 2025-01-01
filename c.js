cc8172710fbb64279d0821fbc953faa533bd16e9
ZEU4S-Dragon <andragon34@gmail.com>
2025-01-01 12:59:23 +0300

    cabdalla

---
+++ c.js
@@ -0,0 +1,59 @@
+// let studentAge= 15;
+// if (studentAge < 15 ) {
+//     console.log("waa yar tahay")
+// }
+
+// else {
+//     console.log("waa weyn tahay: "+ studentAge +" "+ "SABO"  )
+// }
+
+// for loop
+// variable condition  increment    last massage
+// for(let x=0; i<10; 1++)  {
+//     console.log(i)
+ 
+// }
+
+// while loop
+// let i=0;
+// while(i<5)
+// {
+//     console.log(i)
+//     i++
+// }
+
+// do while loop
+
+// let i=0;
+// do {
+//     console.log(i)
+//     i++
+// } while(i<3)
+
+//   for in !? Array
+
+
+
+ 
+// function salaam(){
+//     console.log("asc, mudada somaliyeed")
+// }
+
+// salaam()   calling function  inside arrgument
+
+
+// inside the braket + barametir = varaible for function 
+// function salaam(num1,num2){
+// }
+
+
+// paremeter is avariable
+// while argument is value
+
+
+
+
+// arrow function
+// student=()=>{
+
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors:{
+      Primary:  '#000F32',
+      Secondry: '#232323',
+      therty: '#000F32',
+      white: '#FFFFFF'
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
